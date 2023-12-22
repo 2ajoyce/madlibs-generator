c770fdd14c7a5dce918a86aba40597aa18822cca
Aaron Joyce <2ajoyce@gmail.com>
2023-12-22 10:37:48 -0600

    feat(eslint): add new linting rules and adjust indentation
    This commit adds two new TypeScript ESLint rules to enforce consistent indentation and comma dangle in multiline statements in `.eslintrc`. The rules specify 4 spaces for indentation and require a trailing comma for multiline arrays, objects, etc. Also, the diff shows an adjustment of indentation to conform to the newly introduced standards.
    
    Additionally, CI workflow files for GitHub Actions have been updated with clearer step names and environment variables for building the application. Secrets for P2P server settings have been added to the build process environment variables both in merge and pull request workflows, improving configuration management for different deployment environments.
    
    Moreover, the introduction of a `.prettierrc` file suggests that code formatting will be enforced based on the specified preferences, aligning with the ESLint rules.
    
    Finally, `src/PeerManager.ts` has been refactored to use dynamic environment variables for server configuration, replacing hard-coded values. This improves security and flexibility when deploying to various environments.
    
    Significant changes include:
    - New linting rules (`@typescript-eslint/indent`, `@typescript-eslint/comma-dangle`)
    - Workflow jobs now have named steps and include environment variables
    - The addition of `.prettierrc` sets project-wide Prettier formatting options
    - Server URI and key in `PeerManager.ts` are now loaded from environment variables, increasing configurability and security

--- src/PeerManager.ts
+++ src/PeerManager.ts
@@ -1,107 +1,107 @@
 import Peer, { type DataConnection } from 'peerjs'
 import { v4 as uuidv4 } from 'uuid'
 
-const SERVER_URI = 'p2p.2ajoyce.com'
-const SERVER_KEY = 'd41d8cd98f00b204e9800998ecf8427e'
+const SERVER_URI = import.meta.env.VITE_P2P_SERVER_URI
+const SERVER_KEY = import.meta.env.VITE_P2P_SERVER_KEY
 const SERVER_CONNECTION = {
-  host: SERVER_URI,
-  port: 443,
-  ping: 1000 * 15, // 15s ping
-  secure: true,
-  debug: 2, // 2 for normal operation, 3 for debug
-  key: SERVER_KEY
+    host: SERVER_URI,
+    port: 443,
+    ping: 1000 * 15, // 15s ping
+    secure: true,
+    debug: 2, // 2 for normal operation, 3 for debug
+    key: SERVER_KEY,
 }
 class PeerManager {
-  private static instance: PeerManager
-  public peer: Peer | null = null
-  private connections: Record<string, DataConnection> = {}
+    private static instance: PeerManager
+    public peer: Peer | null = null
+    private connections: Record<string, DataConnection> = {}
 
-  private constructor () {}
+    private constructor() {}
 
-  public static getInstance (): PeerManager {
-    // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
-    if (!PeerManager.instance) {
-      PeerManager.instance = new PeerManager()
-    }
-    return PeerManager.instance
-  }
-
-  public createPeer (callback?: (peerId: string) => void): void {
-    if (this.peer == null) {
-      const id = uuidv4()
-      this.peer = new Peer(id, SERVER_CONNECTION)
-
-      this.peer.on('open', () => {
-        console.log('Created Peer: ', this.peer?.id)
-        if (callback != null && this.peer != null) {
-          callback(this.peer.id)
+    public static getInstance(): PeerManager {
+        // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+        if (!PeerManager.instance) {
+            PeerManager.instance = new PeerManager()
         }
-      })
+        return PeerManager.instance
+    }
 
-      this.peer.on('error', (err) => {
-        console.error('Peer error:', err)
-      })
-      this.peer.on('connection', (conn) => {
-        console.log(`Incoming connection from: ${conn.connectionId}`)
+    public createPeer(callback?: (peerId: string) => void): void {
+        if (this.peer == null) {
+            const id = uuidv4()
+            this.peer = new Peer(id, SERVER_CONNECTION)
+
+            this.peer.on('open', () => {
+                console.log('Created Peer: ', this.peer?.id)
+                if (callback != null && this.peer != null) {
+                    callback(this.peer.id)
+                }
+            })
+
+            this.peer.on('error', (err) => {
+                console.error('Peer error:', err)
+            })
+            this.peer.on('connection', (conn) => {
+                console.log(`Incoming connection from: ${conn.connectionId}`)
+                conn.on('open', () => {
+                    console.log('Connection established')
+                })
+                conn.on('data', (data) => {
+                    console.log('Received data:', data)
+                })
+                conn.on('error', (err) => {
+                    console.error('Connection error:', err)
+                })
+            })
+        } else {
+            console.log('Reusing existing Peer: ', this.peer.id)
+            if (callback != null) {
+                callback(this.peer.id)
+            }
+        }
+    }
+
+    public connectToPeer(peerId: string, callback?: () => void): void {
+        if (this.peer == null) {
+            console.error('Peer is not initialized')
+            return
+        }
+
+        const conn = this.peer.connect(peerId)
         conn.on('open', () => {
-          console.log('Connection established')
-        })
-        conn.on('data', (data) => {
-          console.log('Received data:', data)
+            console.log('Connection established with: ', peerId)
+            this.connections[peerId] = conn
+            if (callback != null) {
+                callback()
+            }
         })
         conn.on('error', (err) => {
-          console.error('Connection error:', err)
+            console.log('Connection error:', err)
         })
-      })
-    } else {
-      console.log('Reusing existing Peer: ', this.peer.id)
-      if (callback != null) {
-        callback(this.peer.id)
-      }
-    }
-  }
-
-  public connectToPeer (peerId: string, callback?: () => void): void {
-    if (this.peer == null) {
-      console.error('Peer is not initialized')
-      return
+        conn.on('close', () => {
+            console.log('Connection closed')
+        })
+        conn.on('iceStateChanged', () => {
+            console.log('Ice State Changed')
+        })
     }
 
-    const conn = this.peer.connect(peerId)
-    conn.on('open', () => {
-      console.log('Connection established with: ', peerId)
-      this.connections[peerId] = conn
-      if (callback != null) {
-        callback()
-      }
-    })
-    conn.on('error', (err) => {
-      console.log('Connection error:', err)
-    })
-    conn.on('close', () => {
-      console.log('Connection closed')
-    })
-    conn.on('iceStateChanged', () => {
-      console.log('Ice State Changed')
-    })
-  }
-
-  public sendToPeer (peerId: string, message: any): void | Promise<void> {
-    // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
-    if (this.connections[peerId]) {
-      return this.connections[peerId].send(message)
-    } else {
-      console.error('No connection found for the given peerId')
+    public sendToPeer(peerId: string, message: any): void | Promise<void> {
+        // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+        if (this.connections[peerId]) {
+            return this.connections[peerId].send(message)
+        } else {
+            console.error('No connection found for the given peerId')
+        }
     }
-  }
 
-  public destroyPeer (): void {
-    if (this.peer != null) {
-      this.peer.destroy()
-      this.peer = null
+    public destroyPeer(): void {
+        if (this.peer != null) {
+            this.peer.destroy()
+            this.peer = null
+        }
+        this.connections = {}
     }
-    this.connections = {}
-  }
 }
 
 export default PeerManager
