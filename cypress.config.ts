49993aca6c310f305899763f8e17393c53530f99
Aaron Joyce <2ajoyce@gmail.com>
2023-12-25 22:05:01 -0600

    feat(testing): exclude specific patterns from Cypress spec files
    Added 'excludeSpecPattern' to Cypress configuration to prevent running tests in certain directories. This change improves test suite performance by skipping files within the 'getting-started' and 'advanced-examples' subdirectories, as well as any additional spec files nested deeper than two levels.

--- cypress.config.ts
+++ cypress.config.ts
@@ -6,6 +6,11 @@ export default defineConfig({
         setupNodeEvents(on, config) {
             console.debug('Impliment node event listeners here', on, config)
         },
+        excludeSpecPattern: [
+          '*/*/**/spec.cy.ts',
+          '*/**cypress/e2e/1-getting-started',
+          '*/**/cypress/e2e/2-advanced-examples'
+      ],
     },
 
     component: {
